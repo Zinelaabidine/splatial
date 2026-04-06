cd4ba51d24a98df265591103d09b4eec5d45cd39
Zinelaabidine NADIR <z.nadir@ymail.com>
2026-04-06 16:25:30 +0300

    feat(terraform): add private s3 bucket with versioning and encryption

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,3 +8,19 @@ output "hosted_zone_name" {
   value       = data.aws_route53_zone.this.name
 
 }
+
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket for the static site"
+  value       = aws_s3_bucket.site.bucket
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket for the static site"
+  value       = aws_s3_bucket.site.arn
+
+}
+
+output "s3_bucket_regional_domain_name" {
+  description = "The regional domain name of the S3 bucket for the static site"
+  value       = aws_s3_bucket.site.bucket_regional_domain_name
+}
